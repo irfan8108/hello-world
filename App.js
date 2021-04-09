c74b9f5c6f46dc1af0ecd58282127ddb8068b818
amiss_engineer <amiss.engineer@gmail.com>
2021-04-10 01:48:05 +0530

    seprate screen

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+
+import Splash from './app/screens/Splash';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={require('./assets/logo.png')} style={styles.logo} />
-      <Text style={{fontSize:25, color: '#000', fontWeight:'bold', letterSpacing:11}}>Engineer</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Splash/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
